c3ffbda3b348e526d920d082e8c20ab5bbeb1baf
Isabella Ong <isabellaong@gmail.com>
2016-06-19 17:15:04 +0800

    adds finished show pages

--- app/controllers/sake_controller.rb
+++ app/controllers/sake_controller.rb
@@ -37,19 +37,19 @@ class SakeController < ApplicationController
 
     ["吟","醸"])
 
-    honjozo_images = Image.new('ginjo.jpg', 'ginjo2.jpg', 'ginjo3.jpg')
+    honjozo_images = Image.new('honjozo.jpg', 'honjozo2.jpg', 'honjozo3.jpg')
 
     honjozo_headings = Heading.new(
     'HONJOZO',
-    'At least 40% of the outer rice layers polished away.',
-    "Ginjos tend to be crowd pleasers: they are smooth, easy drinking, and have great balance.")
+    ' To qualify as honjozo, the weight of the added alcohol must be no more than 10% of the weight of the sake rice used in brewing.',
+    "Honjozos are much lighter on the palate")
 
     honjozo_texts = Text.new(
-    "Honjozo (本醸造) also uses rice that has been polished to at least 70% (as with junmai above). However, honjozo by definition contains a small amount of distilled brewers alcohol, which is added to smooth out the flavor and aroma of the sake. Honjozo sakes are often light and easy to drink, and can be enjoyed both warm or chilled.",
+    "Honjozo (本醸造) also uses rice that has been polished to at least 70% (as with junmai above). However, honjozo by definition contains a small amount of distilled brewers alcohol, which is added to smooth out the flavor and aroma of the sake. Honjozo sakes are often light and easy to drink, and can be enjoyed both warm or chilled. Many sake drinkers hear the definition and assume that honjozo is a sweeter fortified product with alcohol added during fermentation, or that it's necessarily higher in alcohol than junmai, or that it's inferior to junmai, because alcohol is added. All three of these theories are false.",
 
-    "Ginjo sakes are relatively new in the sake world—they weren't widely produced until about 50 years ago. At that time, sake had an image of being sweet and flabby. Sake brewers gradually began polishing the rice further as an experiment, and found the result to be lighter and drier sakes that were extremely enjoyable. Ginjo sakes have a minimum of 60% seimaibuai, or percentage of rice remaining after polishing. Because this is only a minimum, many brewers polish their rice for ginjo sakes past 60% to achieve a smoother, cleaner style. Today, even though the consumption of sake may be on the decline in Japan, the consumption of ginjo grade sakes is still increasing.",
+    "Within the world of premium sake, any labels that do not explicitly state “junmai” on them can be assumed “honjozo”. Honjozo is sake that has a small amount of brewer's alcohol added to the fermenting sake mash. Honjozo sake is not inherently sweet—the alcohol is generally added after the yeast has completed fermenting the sugar in the sake. Although makers of bulk sake do often add large quantities of alcohol to increase overall volume, honjozo sake producers don't add much.",
 
-    "Even though the difference between junmai and ginjo can be as little as a 10% polishing change, the change in aroma and flavor can be quite significant. Junmais have subdued aromas and richer body; ginjos, on the other hand, have more expressive aromas of fruit and floral notes, with a softer finish. Ginjos tend to be crowd pleasers: they are smooth, easy drinking, and have great balance. Ginjos also are often a great value; fantastic ginjo sake can be found in the $50-70 range on wine lists or $20-35 retail. Some well priced, excellent-quality ginjos to look out for include Nanbu Bijin Junmai Ginjo, Jokigen Junmai Ginjo*, and Dassai 50 Junmai Ginjo.",
+    "In a blind tasting, you probably couldn't correctly tell a junmai sake from a honjozo every time. The flavor is more more dependent on what rice and yeast were used than on whether a sake is a junmai or honjozo. But honjozos do have certain qualities that stand out. While junmai sakes have a fuller texture, honjozos are much lighter on the palate, so they're easier to drink throughout an entire meal, or an entire evening. Honjozos, on the other hand, are generally less complex and maintain the same aroma/flavor profile as the glass sits. For this reason, honjozos are often good candidates for gentle warming, as they do not have the subtleties of junmai that can get lost with the slight raise in temperature.",
 
     ["本","醸","造"])
 
@@ -63,6 +63,7 @@ class SakeController < ApplicationController
       @overview = daiginjo_texts.overview
       @text_1 = daiginjo_texts.text_1
       @text_2 = daiginjo_texts.text_2
+      @characters = daiginjo_texts.characters
 
     elsif info.to_i == 2
       @main_image = ginjo_images.main
@@ -74,6 +75,7 @@ class SakeController < ApplicationController
       @overview = ginjo_texts.overview
       @text_1 = ginjo_texts.text_1
       @text_2 = ginjo_texts.text_2
+      @characters = ginjo_texts.characters
 
     elsif info.to_i == 3
       @main_image = honjozo_images.main
@@ -85,28 +87,7 @@ class SakeController < ApplicationController
       @overview = honjozo_texts.overview
       @text_1 = honjozo_texts.text_1
       @text_2 = honjozo_texts.text_2
-
-    # elsif info == 'serving'
-    #   @main_image = daiginjo_images.main
-    #   @image_1 = daiginjo_images.image_1
-    #   @image_2 = daiginjo_images.image_2
-    #   @title = daiginjo_headings.title
-    #   @heading_1 = daiginjo_headings.heading_1
-    #   @heading_2 = daiginjo_headings.heading_2
-    #   @overview = daiginjo_texts.overview
-    #   @text_1 = daiginjo_texts.text_1
-    #   @text_2 = daiginjo_texts.text_2
-    #
-    # elsif info == 'birth_of_sake'
-    #   @main_image = daiginjo_images.main
-    #   @image_1 = daiginjo_images.image_1
-    #   @image_2 = daiginjo_images.image_2
-    #   @title = daiginjo_headings.title
-    #   @heading_1 = daiginjo_headings.heading_1
-    #   @heading_2 = daiginjo_headings.heading_2
-    #   @overview = daiginjo_texts.overview
-    #   @text_1 = daiginjo_texts.text_1
-    #   @text_2 = daiginjo_texts.text_2
+      @characters = honjozo_texts.characters
     end
 
   end
@@ -132,3 +113,25 @@ class Text
     @overview, @text_1, @text_2, @characters = overview, txt_1, txt_2, characters
   end
 end
+
+# elsif info == 'serving'
+#   @main_image = daiginjo_images.main
+#   @image_1 = daiginjo_images.image_1
+#   @image_2 = daiginjo_images.image_2
+#   @title = daiginjo_headings.title
+#   @heading_1 = daiginjo_headings.heading_1
+#   @heading_2 = daiginjo_headings.heading_2
+#   @overview = daiginjo_texts.overview
+#   @text_1 = daiginjo_texts.text_1
+#   @text_2 = daiginjo_texts.text_2
+#
+# elsif info == 'birth_of_sake'
+#   @main_image = daiginjo_images.main
+#   @image_1 = daiginjo_images.image_1
+#   @image_2 = daiginjo_images.image_2
+#   @title = daiginjo_headings.title
+#   @heading_1 = daiginjo_headings.heading_1
+#   @heading_2 = daiginjo_headings.heading_2
+#   @overview = daiginjo_texts.overview
+#   @text_1 = daiginjo_texts.text_1
+#   @text_2 = daiginjo_texts.text_2
